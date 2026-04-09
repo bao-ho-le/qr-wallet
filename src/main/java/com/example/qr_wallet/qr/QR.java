0ba00a1dee9fd0cc5b7cfdace0129365d6b5eaee
bao-ho-le <levo24k@gmail.com>
2026-04-09 16:00:29 +0700

    init project

---
+++ src/main/java/com/example/qr_wallet/qr/QR.java
@@ -0,0 +1,5 @@
+package com.example.qr_wallet.qr;
+
+public class QR {
+
+}
---
+++ src/test/java/com/example/qr_wallet/QrWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qr_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
